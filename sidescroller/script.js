845a4eb3ae6ae3dc12966a4950c227ef32dc800e
c0zymaneASC <seanalexander500@gmail.com>
2019-08-07 15:31:32 -0400

    A lot lmao

--- sidescroller/script.js
+++ sidescroller/script.js
@@ -4,13 +4,32 @@ var actionMan;
 var bkg;
 
 function preload() {
+  var walkAnimation = loadAnimation(
+    "./images/hero_walk_1.png",
+    "./images/hero_walk_2.png",
+    "./images/hero_walk_3.png",
+    "./images/hero_walk_4.png",
+    "./images/hero_walk_5.png",
+    "./images/hero_walk_6.png");
+
+    actionMan = createSprite(0, 0, 85, 85);
+    actionMan.addAnimation("walk", walkAnimation);
+    //actionMan.addAnimation("stand", standAnimation);
+  
+    bkg = loadImage('images/background.png');
 }
 
 function setup() {
   var canvas = createCanvas(500, 500);
   canvas.parent('#container');
+
+  actionMan.position.x = width / 2;
+  actionMan.position.y = height - 150;
 }
 
 function draw() {
+  background(80);
+  image(bkg, 0, 0);
+  drawSprites();
 }
 
--- spaceshooter/script.js
+++ spaceshooter/script.js
@@ -9,6 +9,8 @@ function preload() {
   var shipImage = loadImage('images/spaceship.png');
   var shipImageFire = loadImage('images/spaceship-fire.png');
 
+  astroid = createSprite(0, 0, 100, 100);
+
   spaceShip = createSprite(0, 0, 85, 85);
   spaceShip.addImage('rest', shipImage);
   spaceShip.addImage('move', shipImageFire);
@@ -19,24 +21,38 @@ function setup() {
   var canvas = createCanvas(500, 500);
   canvas.parent('#container');
 
+  astroid.shapeColor = color("red");
+  astroid.position.x = width;
+  astroid.position.y = height / 2;
+
   spaceShip.position.x = width / 2;
   spaceShip.position.y = height / 2;
 
   spaceShip.changeImage('rest');
+
+  spaceShip.maxSpeed = 5;
 }
 
 function draw() {
  background(80);
+ 
 
  if(mouseIsPressed){
    spaceShip.changeImage('move');
+   spaceShip.rotateToDirection = true;
+   spaceShip.attractionPoint(1, mouseX, mouseY);
  } else {
    spaceShip.changeImage('rest');
  }
+
+ spaceShip.collide(astroid);
  drawSprites();
 }
 
 var resetButton = document.getElementById('resetButton');
 resetButton.addEventListener('click', function () {
+  spaceShip.position.x = 50;
+  spaceShip.position.y = 50;
+  spaceShip.rotation = 0;
 
 });
