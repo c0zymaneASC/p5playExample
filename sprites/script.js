085d547227543c11e152a6efeaa1469509cc53bc
c0zymaneASC <seanalexander500@gmail.com>
2019-08-07 14:11:30 -0400

    Mouse movement

--- sprites/script.js
+++ sprites/script.js
@@ -7,7 +7,27 @@ function setup() {
   var canvas = createCanvas(500, 500);
   canvas.parent('#container');
 
+  spr1 = createSprite(0, 0, 50, 50);
+  spr1.shapeColor = color("cyan");
+
+  spr1.rotateToDirection = true;
+  
+
+  spr1.velocity.y = 1;
+
+  spr1.position.x = width / 2;
+  spr1.position.y = height / 2;
+  spr1.maxSpeed = 5;
 }
 
 function draw() {
+  background(80);
+  drawSprites();
+  
+  if (mouseIsPressed) {
+    spr1.attractionPoint(0.5, mouseX, mouseY);
+  } else {
+    spr1.setSpeed(0);
+  }
+
 }
