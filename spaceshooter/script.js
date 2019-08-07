a4d885defd1d8f9eaad91efea1a0c6c2395b82a2
c0zymaneASC <seanalexander500@gmail.com>
2019-08-07 14:36:21 -0400

    Ship animation

--- spaceshooter/script.js
+++ spaceshooter/script.js
@@ -6,14 +6,34 @@ var bkg;
 var collideCount = 0;
 
 function preload() {
+  var shipImage = loadImage('images/spaceship.png');
+  var shipImageFire = loadImage('images/spaceship-fire.png');
+
+  spaceShip = createSprite(0, 0, 85, 85);
+  spaceShip.addImage('rest', shipImage);
+  spaceShip.addImage('move', shipImageFire);
+
 }
 
 function setup() {
-  var canvas = createCanvas(300, 300);
+  var canvas = createCanvas(500, 500);
   canvas.parent('#container');
+
+  spaceShip.position.x = width / 2;
+  spaceShip.position.y = height / 2;
+
+  spaceShip.changeImage('rest');
 }
 
 function draw() {
+ background(80);
+
+ if(mouseIsPressed){
+   spaceShip.changeImage('move');
+ } else {
+   spaceShip.changeImage('rest');
+ }
+ drawSprites();
 }
 
 var resetButton = document.getElementById('resetButton');
